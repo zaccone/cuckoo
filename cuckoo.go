8ca3e4e6a1c9fe9ff7c8ea27338c0c728862d649
zaccone <marek.denis+zaccone@gmail.com>
2019-02-16 11:01:11 +0000

    Initial commit of Cuckoo hash table.
    Basic structure of the Cuckoo hash table.

---
+++ cuckoo.go
@@ -0,0 +1,120 @@
+package cuckoo
+
+import (
+	"errors"
+	"fmt"
+)
+
+type hashFn func(key uint64, size uint64) uint64
+
+// ErrTableEmpty is a dedicated error indicating the element cannot be added due to collisions
+var ErrTableEmpty = errors.New("Cuckoo table is full")
+
+func yinHash(key uint64, size uint64) uint64 {
+	return key % size
+}
+
+func yangHash(key uint64, size uint64) uint64 {
+	return (key / size) % size
+}
+
+// Item is an object stores in the Cuckoo hash object
+type Item struct {
+	Key   uint64
+	Value string
+}
+
+func (i *Item) String() string {
+	return fmt.Sprintf("Item key: %v, Value: %v", i.Key, i.Value)
+}
+
+// Cuckoo is a structure representing whole hash object
+// For now lets make it of key integer and value string
+// TODO(mde): store configurable hash functions, maybe somebody wants to use their own?
+type Cuckoo struct {
+	size uint64
+	yin  []*Item
+	yang []*Item
+
+	yinHash  hashFn
+	yangHash hashFn
+}
+
+// New returns new Cuckoo hash object with the size initialized via the parameter
+func New(size uint64) *Cuckoo {
+	return &Cuckoo{size, make([]*Item, size), make([]*Item, size), yinHash, yangHash}
+}
+
+// Insert inserts or replaces element stored under the key.
+func (c *Cuckoo) Insert(key uint64, value string) bool {
+
+	// try first of two possible locations
+	yinH := c.yinHash(key, c.size)
+	if c.yin[yinH] == nil {
+		c.yin[yinH] = &Item{Key: key, Value: value}
+		return true
+	}
+
+	// try second of two possible locations
+	yangH := c.yangHash(key, c.size)
+	if c.yang[yangH] == nil {
+		c.yang[yangH] = &Item{Key: key, Value: value}
+		return true
+	}
+
+	// Try misplacing element from yin and see if that is possible. If so, enter the element into yin
+
+	if c.Insert(yinH, c.yin[yinH].Value) {
+		c.yin[yinH] = &Item{Key: key, Value: value}
+		return true
+	}
+	// Try misplacing element from yang and see if that is possible. If so, enter the element into yang
+
+	if c.Insert(yangH, c.yang[yangH].Value) {
+		c.yin[yangH] = &Item{Key: key, Value: value}
+		return true
+	}
+
+	// Apparently we couldn't find enough room in either of the hashes, return nil, as the table is considered 'full'
+	return false // for now, lol
+
+}
+
+// Lookup finds element stored as key and returns pointer to this element or nil if the element does not exist.
+func (c *Cuckoo) Lookup(key uint64) *Item {
+	yin := c.yin[c.yinHash(key, c.size)]
+	if ok(yin, key) {
+		return yin
+	}
+
+	yang := c.yang[yangHash(key, c.size)]
+	if ok(yang, key) {
+		return yang
+	}
+	return nil
+}
+
+// Delete removes element from the hashtable and returns true if the element was stored, false otherwise.
+func (c *Cuckoo) Delete(key uint64) bool {
+	return false
+}
+
+// Debug prints values from both yin ang yang tables
+func (c *Cuckoo) Debug() {
+	fmt.Println("Yin: ")
+	for idx, item := range c.yin {
+		fmt.Printf("%d: %s", idx, item.String())
+	}
+	fmt.Println("")
+
+	fmt.Println("Yang: ")
+	for idx, item := range c.yang {
+		fmt.Printf("%d: %s", idx, item.String())
+	}
+	fmt.Println("")
+}
+
+// helper function to extract Item if it really matches
+func ok(item *Item, key uint64) bool {
+	return item != nil && item.Key == key
+}
---
+++ cuckoo_test.go
@@ -0,0 +1,147 @@
+package cuckoo
+
+import "testing"
+
+const TableSize = 11
+
+func itemCompare(i1 *Item, i2 *Item) bool {
+	if i1 == nil && i2 == nil {
+		return true
+	}
+
+	return i1.Key == i2.Key && i1.Value == i2.Value
+}
+
+func TestLookupNil(t *testing.T) {
+	cuckoo := New(TableSize)
+
+	for k := uint64(0); k < 100; k++ {
+		if cuckoo.Lookup(k) != nil {
+			t.Errorf("Expected value nil for key: %d", k)
+		}
+	}
+
+}
+
+func TestInsertOnEmpty(t *testing.T) {
+	cuckoo := New(TableSize)
+
+	item := &Item{Key: 13, Value: "marek"}
+
+	if cuckoo.Lookup(item.Key) != nil {
+		t.Errorf("Expected nil during first Cuckoo.Lookup()")
+	}
+
+	if cuckoo.Insert(item.Key, item.Value) == false {
+		t.Errorf("Expected true while inserting, got false")
+	}
+
+	stored := cuckoo.Lookup(item.Key)
+
+	if stored == nil {
+		t.Errorf("Expected !nil during second Cuckoo.Lookup()")
+	}
+
+	if stored.Key != item.Key || stored.Value != item.Value {
+		t.Errorf("Item mismatch item: %s vs stored: %s", item.String(), stored.String())
+	}
+
+}
+
+func TestInsertWithSameKey(t *testing.T) {
+
+	cuckoo := New(TableSize)
+
+	i1 := &Item{Key: 3, Value: "three"}
+	i2 := &Item{Key: 36, Value: "thirty six"}
+
+	if cuckoo.yinHash(i1.Key, TableSize) != cuckoo.yangHash(i2.Key, TableSize) {
+		t.Errorf("Key equality is a prerequisite for collision resolution test: yinHash %d, yangHash: %d",
+			cuckoo.yinHash(i1.Key, TableSize), cuckoo.yangHash(i2.Key, TableSize))
+	}
+
+	cuckoo.Insert(i1.Key, i1.Value)
+	cuckoo.Insert(i2.Key, i2.Value)
+
+	ci1 := cuckoo.Lookup(i1.Key)
+
+	if itemCompare(i1, ci1) == false {
+		t.Errorf("Item 1 mismatch: original item %s, fetched item: %s", i1.String(), ci1.String())
+	}
+
+	ci2 := cuckoo.Lookup(i2.Key)
+
+	if itemCompare(i2, ci2) == false {
+		t.Errorf("Item 2 mismatch: original item %s, fetched item: %s", i2.String(), ci2.String())
+	}
+
+}
+
+/* TestInsertWithEviction tests inserting and lookups where the eviction must happen. For the standard hash function we will have following values hashing to values:
+
+Key  YinFn YangFn
+105	 6	   9
+3	 3	   0
+36	 3	   3
+39	 6	   3
+
+*/
+func TestInsertWithEviction(t *testing.T) {
+	cuckoo := New(TableSize)
+
+	// Value 3 hashes to
+	i1 := &Item{Key: 3, Value: "three"}
+	i2 := &Item{Key: 36, Value: "thirty six"}
+	i3 := &Item{Key: 39, Value: "thirty nine"}
+	i4 := &Item{Key: 105, Value: "hundred and five"}
+
+	cuckoo.Insert(i1.Key, i1.Value)
+	cuckoo.Insert(i2.Key, i2.Value)
+	cuckoo.Insert(i3.Key, i3.Value)
+	cuckoo.Insert(i4.Key, i4.Value)
+
+	ci1 := cuckoo.Lookup(i1.Key)
+
+	if itemCompare(i1, ci1) == false {
+		t.Errorf("Item 1 mismatch: original item %s, fetched item: %s", i1.String(), ci1.String())
+	}
+
+	ci2 := cuckoo.Lookup(i2.Key)
+
+	if itemCompare(i2, ci2) == false {
+		t.Errorf("Item 2 mismatch: original item %s, fetched item: %s", i2.String(), ci2.String())
+	}
+
+	ci3 := cuckoo.Lookup(i3.Key)
+
+	if itemCompare(i3, ci3) == false {
+		t.Errorf("Item 3 mismatch: original item %s, fetched item: %s", i3.String(), ci3.String())
+	}
+
+	ci4 := cuckoo.Lookup(i4.Key)
+
+	if itemCompare(i4, ci4) == false {
+		t.Errorf("Item 3 mismatch: original item %s, fetched item: %s", i4.String(), ci4.String())
+	}
+
+}
+
+func TestEmptyLookup(t *testing.T) {
+	cuckoo := New(TableSize)
+
+	// Value 3 hashes to
+	i1 := &Item{Key: 3, Value: "three"}
+	i2 := &Item{Key: 36, Value: "thirty six"}
+	i3 := &Item{Key: 39, Value: "thirty nine"}
+	i4 := &Item{Key: 105, Value: "hundred and five"}
+
+	cuckoo.Insert(i1.Key, i1.Value)
+	cuckoo.Insert(i2.Key, i2.Value)
+	cuckoo.Insert(i3.Key, i3.Value)
+	cuckoo.Insert(i4.Key, i4.Value)
+
+	if cuckoo.Lookup(1) != nil {
+		t.Error("Expected to not found any element.")
+	}
+
+}
